71007c6a672b33a2889f1bcc7e532c2ad06931ae
dnflsclsrn1 <wnsgur765z@naver.com>
2021-11-21 22:28:33 +0900

    feat:casecade

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     private EntityManager em;
@@ -18,18 +17,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("");
 
-            em.persist(member);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team = new Team();
-            team.setName("teamA");
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            // 연관 관계가 바뀌어서 애매할 수 있다.
-            team.getMembers().add(member);
+            em.persist(parent);
 
-            em.persist(team);
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
             tx.commit();
         } catch (Exception e) {
