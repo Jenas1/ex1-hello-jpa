41347d95dc4ef8a8ba8b96f8516d6d9f6bb34726
dnflsclsrn1 <wnsgur765z@naver.com>
2021-11-30 23:04:01 +0900

    feat: 값타입 예제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
     private EntityManager em;
@@ -17,21 +19,54 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address = new Address("city", "street", "100");
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(address);
+            member.setHomeAddress(new Address("homeCity", "street", "1000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "1000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "1000"));
+
             em.persist(member);
 
-            Address copyAddress = new Address(address.getCity(), address.getStreet(),address.getZipcode());
+            em.flush();
+            em.clear();
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setHomeAddress(copyAddress);
-            em.persist(member2);
+            System.out.println("============= START =============");
+            Member findMember = em.find(Member.class, member.getId());
 
-            //member.getHomeAddress().setCity("newCity");
+            // 지연로딩인 것이다.
+//            List<Address> addressesHistory = findMember.getAddressHistory();
+//            for (Address address: addressesHistory) {
+//                System.out.println("address =: "+ address.getCity());
+//            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String favoriteFood : favoriteFoods) {
+                System.out.println("address =: "+ favoriteFood);
+            }
+
+
+            // city 를 바꾸고 싶으면 통으로 갈아 껴주어야 한다. 값타입은
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            //값타입 업데이트
+            // 치킨 > 한식  통으로 갈아끼워야한다. 지우고
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            //주소를 바꾸는거 기본적으로 컬렉션은 리무브가 이퀄스를 찾는다. 제대로 넣어야함
+            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "1000"));
+            findMember.getAddressHistory().add(new AddressEntity("newCity2", "street", "1000"));
+
+
+
+            //member.getHomeAddress().setCity("newCity");설정안해주면이렇게 변경해줘야함!
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,6 +1,10 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 @Entity
@@ -19,6 +23,25 @@ public class Member {
     // 주소
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    ) //매핑정보
+    @Column(name = "FOOD_NAME") // Set<타입> 타입이 정의한게 하나기때문에 이렇게 컬럼명을 정의해준다!
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns =
+//        @JoinColumn(name = "MEMBER_ID")
+//    )  //매핑정보
+//    private List<Address> addressHistory = new ArrayList <>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)//실무에서 사용 1대다 단방향 매핑 값타입 보다 훨씬 활용도가 올라간다.
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
+
+
 
     public Long getId() {
         return id;
@@ -52,5 +75,28 @@ public class Member {
         this.homeAddress = homeAddress;
     }
 
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
 
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+//    public List<Address> getAddressHistory() {
+//        return addressHistory;
+//    }
+//
+//    public void setAddressHistory(List<Address> addressHistory) {
+//        this.addressHistory = addressHistory;
+//    }
+
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
+    }
 }
