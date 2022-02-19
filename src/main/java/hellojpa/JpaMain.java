1509e918e6312f85366a909cf22b124c98da83d7
dnflsclsrn1 <wnsgur765z@naver.com>
2022-02-19 22:33:56 +0900

    feat:쿼리언어

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,54 +19,25 @@ public class JpaMain {
         tx.begin();
 
         try {
+//            //jPAL
+//            List<Member> result = em.createQuery(
+//                    "select m From Member m where m.username like '%kim%'",
+//                    Member.class
+//            ).getResultList();
+//            // m 은 맴버자체를 가져오는것 !
+//            for (Member member: result) {
+//                System.out.println("member = "+ member);
+//            }
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "1000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "1000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "1000"));
-
             em.persist(member);
 
-            em.flush();
-            em.clear();
+            //flush > commit, query날라갈때는 동작한다.
+            //dbconn.excuteQuery>> 요시점엔는 결과0 이다. 이런경우는 강제로 플러쉬를 해줘야 한다
+            em.createNativeQuery("select Member_ID, city, street, zipcode, USERNAME from MEMBER")
+                            .getResultList();
 
-            System.out.println("============= START =============");
-            Member findMember = em.find(Member.class, member.getId());
-
-            // 지연로딩인 것이다.
-//            List<Address> addressesHistory = findMember.getAddressHistory();
-//            for (Address address: addressesHistory) {
-//                System.out.println("address =: "+ address.getCity());
-//            }
-
-            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-            for (String favoriteFood : favoriteFoods) {
-                System.out.println("address =: "+ favoriteFood);
-            }
-
-
-            // city 를 바꾸고 싶으면 통으로 갈아 껴주어야 한다. 값타입은
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            //값타입 업데이트
-            // 치킨 > 한식  통으로 갈아끼워야한다. 지우고
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            //주소를 바꾸는거 기본적으로 컬렉션은 리무브가 이퀄스를 찾는다. 제대로 넣어야함
-            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "1000"));
-            findMember.getAddressHistory().add(new AddressEntity("newCity2", "street", "1000"));
-
-
-
-            //member.getHomeAddress().setCity("newCity");설정안해주면이렇게 변경해줘야함!
 
             tx.commit();
         } catch (Exception e) {
